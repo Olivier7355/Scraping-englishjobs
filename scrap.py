51884ad026e30f83260af258d78a0783ffd6c7ba
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-03-07 18:32:41 +0000

    Last version

--- scrap.py
+++ scrap.py
@@ -1,1 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
+pageNumber = 1
+
+while (pageNumber < 2) :
+    if (pageNumber == 1) : 
+        URL = "https://englishjobs.es/in/barcelona?q=python"
+    else :
+        URL = "https://englishjobs.es/in/barcelona?q=python&page=" + str(pageNumber)
+
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.findAll(class_="row job js-job")
+
+    for job in results :
+
+        position = job.find(class_="title").text.strip()
+        if 'Python' in position :
+            link = 'https://englishjobs.es/' + job.find('a', class_="js-joblink joblink js-external")["href"]
+            company = job.find('li').text
+            content = job.find('p').text.replace('\n', '')
+
+            publish = job.find('li').next_sibling.next_sibling.next_sibling.next_sibling.text
+            publish = '2023 ' + publish
+            publishingDate = date_object = datetime.strptime(publish, "%Y %B %d")
+
+            if publishingDate > (datetime.now() - timedelta(days=7)) :
+
+                print(position)
+                print(link)
+                print(company)
+                print(content.strip())
+                print(publishingDate)
+                print('--------------------------------------------------')
+            
+    pageNumber += 1
